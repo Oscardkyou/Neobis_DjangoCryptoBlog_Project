bc2181bf553d55d623be0ba1fabe25fa10e33d98
Dauren <dauren2050@yandex.ru>
2024-05-15 08:23:33 +0600

    Third commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
     path("category/<category>/", views.blog_category, name="blog_category"),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
